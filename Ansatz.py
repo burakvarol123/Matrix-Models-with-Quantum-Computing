e26ee08fe0924ae820d718e4930fc3211f7aef19
Burak <varolbur@hu-berlin.de>
2023-11-07 21:53:54 +0100

    tue,7.Nov. Added module ansatz, configuring varqite and hamilton module

---
+++ Ansatz.py
@@ -0,0 +1,96 @@
+from qiskit import *
+from qiskit.circuit import Parameter
+import numpy as np
+
+
+def ansatz_review_exact(N, depth):  # i only use ry gates now, exactly like in the paper
+    circuit = QuantumCircuit(N,N)
+    thetas = []
+    for l in range(N * (depth + 1)):
+        thetas.append(Parameter('θ_' + str(l)))
+    counter_3 = 0
+    for i in range(depth):
+        counter_2 = 0
+        for l in range(N):
+            if l + counter_2 + counter_3 < len(thetas):
+                circuit.ry(thetas[l + counter_2 + counter_3 + N], l + counter_2)
+        counter_3 = counter_3 + N
+        for p in range(N):
+            circuit.cx(p, (p + 1) % N)
+        circuit.barrier()
+    counter = 0
+    for j in range(N):
+        circuit.ry(thetas[j], j)
+        counter = counter + 1
+    for k in range(N):
+        if k + int(N / 2) < N:
+            circuit.cx(k, (k + int(N / 2)))
+    return circuit
+
+
+def ansatz_varqite_rev(N, depth):  # the order is now like the one in the paper
+    if N % 2 != 0:
+        raise ValueError('You cant give uneven total qubit number')
+    circuit = QuantumCircuit(N)
+    thetas = []
+    for l in range(N * (depth + 1)):
+        thetas.append(Parameter('θ_' + str(l)))
+    counter_3 = 0
+    for i in range(depth):
+        counter_2 = 0
+
+        for l in range(N):
+            if l + counter_2 + counter_3 < len(thetas):
+                circuit.ry(thetas[l + counter_2 + counter_3 + N], l + counter_2)
+        counter_3 = counter_3 + N
+        for p in range(N):
+            circuit.cx(p, (p + 1) % N)
+        circuit.barrier()
+    counter = 0
+    for j in range(N):
+        if j + counter < N:
+            circuit.ry(thetas[j + counter], j)
+        if j + 1 + counter < N:
+            circuit.rx(thetas[j + 1 + counter], j)
+        if j + 1 + counter < N:
+            circuit.cx(j, (j + int(N / 2)))
+        counter = counter + 1
+
+    return circuit
+
+
+def ansatz_varqite(N, depth):
+    if N % 2 != 0:
+        raise ValueError('You cant give uneven total qubit number')
+    circuit = QuantumCircuit(N)
+    thetas = []
+    for l in range(N * (depth + 1)):
+        thetas.append(Parameter('θ_' + str(l)))
+    counter = 0
+    for j in range(N):
+        if j + counter < N:
+            circuit.ry(thetas[j + counter], j)
+        if j + 1 + counter < N:
+            circuit.rx(thetas[j + 1 + counter], j)
+        if j + 1 + counter < N:
+            circuit.cx(j, (j + int(N / 2)))
+        counter = counter + 1
+
+    counter_3 = 0
+    for i in range(depth):
+        circuit.barrier()
+        counter_2 = 0
+
+        for l in range(N):
+            if l + counter_2 + counter_3 < len(thetas):
+                circuit.ry(thetas[l + counter_2 + counter_3 + N], l + counter_2)
+        counter_3 = counter_3 + N
+        for p in range(N):
+            circuit.cx(p, (p + 1) % N)
+    return circuit
+
+
+def measu(N):
+    circuit = QuantumCircuit(N, int(N / 2))
+    circuit.measure(np.arange(int(N / 2)-1), int(N / 2)-1)
+    return circuit
--- Hamilton.py
+++ Hamilton.py
@@ -1,34 +1,43 @@
 import numpy as np
 from qiskit.circuit.library import *
-from qiskit.quantum_info import Operator
+import qiskit.quantum_info as qi
+from qiskit import *
+from qiskit.circuit.QuantumCircuit import *
+
+
+def krZ(m, N):
+    matrix = np.array(1)
+    for j in range(N):
+        if m == j:
+            matrix = np.kron(matrix, ZGate())
+        else:
+            matrix = np.kron(matrix, IGate())
+    return matrix
+
+
+def krId(N):
+    matrix = np.array(1)
+    for j in range(N):
+        matrix = np.kron(matrix, IGate())
+    return matrix
+
+
+def ktot(N):
+    def k_m(N, a, m):
+        k = (krId(N) - krZ(m, N)) / 2.0
+        return k
+
+    sum = 0
+    for h in range(N):
+        sum += 2 ** h * k_m(N, 1, h)
+    return sum
 
 
 def Ham(N, a, b):
-    def krZ(m, N):
-        matrix = np.array(1)
-        for j in range(N):
-            if m == j:
-                matrix = np.kron(matrix, ZGate())
-            else:
-                matrix = np.kron(matrix, IGate())
-        return matrix
-
-    def krId(N):
-        matrix = np.array(1)
-        for j in range(N):
-            matrix = np.kron(matrix, IGate())
-        return matrix
-
-    def ktot(N):
-        def k_m(N, a, m):
-            k = (krId(N) - krZ(m, N)) / 2.0
-            return k
-
-        sum = 0
-        for h in range(N):
-            sum += 2 ** h * k_m(N, 1, h)
-        return sum
-
     mat = a ** 2 * np.linalg.matrix_power((0.5 * np.kron(ZGate(), krId(N - 1)) + np.kron(ZGate(), ktot(N - 1))), b)
-    op = Operator(mat)
-    return op
+    return mat
+
+def ham_gate(Ham,N,a,b):
+    qc = QuantumCircuit(2*N)
+    ham = qi.Operator(Ham(N,a,b))
+    return qc.unitary()
