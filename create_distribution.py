dec84ffbc8256dfd1f7bb5a0282bb01a3985a87d
burak <burakbabaop@github.com>
2024-05-19 17:00:58 +0200

    added docstrings, renamed the functions logical, added extra variables to functions, created plot_distribution and quick_compare, moved expectation value calculators to observables.py

--- create_distribution.py
+++ create_distribution.py
@@ -1,176 +1,308 @@
+"""
+create_distribution.py
+
+This module provides functions to perform and compare quantum imaginary time 
+evolution using Qiskit. It includes functionalities for creating distributions 
+using Variational Quantum Imaginary Time Evolution (varQITE) and SciPy's 
+imaginary time evolver, as well as plotting and comparing the results.
+
+Functions:
+- create_distribution_varqite(qubits, depth, hamilton, beta, ansatz, 
+  num_timesteps): Evolves the given initial state using varQITE and returns 
+  the result.
+- create_distribution_scipy(qubits, depth, hamilton, beta, ansatz, 
+  num_timesteps): Evolves the given initial state using SciPy's imaginary time 
+  evolver and returns the result.
+- plot_distribution(evolution_result, sol, qubits, depth, a, g, save, 
+  filename): Visualizes the distribution for given varQITE and SciPy results.
+- quick_compare(evolution_result, sol, qubits, dimension, depth, a, 
+  num_timesteps, g, save, filename): Compares the evolution of the energy 
+  between varQITE and SciPy evolved distributions.
+
+Each function facilitates different aspects of quantum time evolution 
+simulations and their visualization, aiding in the analysis and comparison 
+of results from different methods.
+
+Examples:
+    # Create a distribution using varQITE
+    result_varqite = create_distribution_varqite(
+        qubits=4, depth=2, hamilton=hamiltonian, beta=1.0, 
+        ansatz=quantum_circuit, num_timesteps=10
+    )
+
+    # Create a distribution using SciPy's imaginary time evolver
+    result_scipy = create_distribution_scipy(
+        qubits=4, depth=2, hamilton=hamiltonian, beta=1.0, 
+        ansatz=quantum_circuit, num_timesteps=10
+    )
+
+    # Plot the distribution comparison between varQITE and SciPy results
+    plot_distribution(result_varqite, result_scipy, qubits=4, depth=2, a=0.5, 
+                      g=1.0)
+
+    # Quickly compare the energy evolution between varQITE and SciPy results
+    quick_compare(result_varqite, result_scipy, qubits=4, dimension=1, 
+                  depth=2, a=0.5, num_timesteps=10, g=1.0)
+
+Dependencies:
+- numpy
+- os
+- matplotlib.pyplot
+- pylab
+- typing
+- qiskit
+- utility.py
+"""
 #!/lustre/fs24/group/cqta/bvarol/package/miniconda3/bin/python3.11
 import sys
 module_directory_path = "/lustre/fs24/group/cqta/bvarol/workspace/matrixmodels"
 sys.path.append(module_directory_path)
 
-
-import hamiltonian_2 as hm2
 import numpy as np
-import Ansatz as an
-from qiskit.primitives import Estimator
-from qiskit.algorithms import VarQITE
-from qiskit.algorithms.time_evolvers.variational import ImaginaryMcLachlanPrinciple
-from qiskit.algorithms import TimeEvolutionProblem
-from qiskit.quantum_info import *
-import pylab
-from qiskit.algorithms import SciPyImaginaryEvolver
-from qiskit.quantum_info import Statevector
+import os
 import matplotlib.pyplot as plt
-from utility import generate_combinations
-import os 
-import one_matrix_model as om
+import pylab
+from typing import Any, List
+from qiskit.algorithms import (SciPyImaginaryEvolver, TimeEvolutionProblem, 
+                               VarQITE, TimeEvolutionResult)
 from qiskit.algorithms.gradients import ReverseEstimatorGradient, ReverseQGT
+from qiskit.algorithms.time_evolvers.variational import (
+                            ImaginaryMcLachlanPrinciple, VariationalPrinciple)
+from qiskit.circuit import QuantumCircuit
+from qiskit.primitives import Estimator
+from qiskit.quantum_info import Statevector
+from qiskit.opflow import OperatorBase
+from utility import generate_combinations
 
 
+def create_distribution_varqite(
+        qubits: int, 
+        depth: int, 
+        hamilton: OperatorBase,
+        beta: float, 
+        ansatz: QuantumCircuit,
+        num_timesteps: int) -> TimeEvolutionResult: 
+    """ 
+    Evolves the given initial state according to Variational Quantum 
+    Imaginary Time Evolution. Creates rho = exp(-beta * H)
 
-def create_distribution_2_varqite(qubits, depth, hamilton, beta, ansatz):
-    hamiltonian = hamilton #^ hm2.insert_i(qubits)
-    var_principle = ImaginaryMcLachlanPrinciple(qgt=ReverseQGT(), gradient=ReverseEstimatorGradient())
-    time = (1 /2.0)* beta
-    aux_ops = [hamiltonian]
-    #init_param_values = [np.pi / 2] * qubits + [0] * (depth * 2 * qubits  + qubits + int(depth*2*qubits*(2*qubits-1)/2))
-    init_param_values =  [0] * (depth * qubits + int(depth*qubits*(qubits-1)/2))
-    evolution_problem = TimeEvolutionProblem(hamiltonian, time, aux_operators=aux_ops)
-    var_qite = VarQITE(ansatz, init_param_values, var_principle, Estimator(),num_timesteps= 300)
-    evolution_result = var_qite.evolve(evolution_problem)
+    Args:
+        qubits (int): The total numbers of qubits of the evolved system.
+        depth (int): depth
+        hamilton (OperatorBase): The Hamilton operator which will lead the 
+        evolution. 
+        beta (float): Inverse Temparature
+        ansatz (QuantumCircuit): The initial ansatz for the parameteritzed 
+        circuit
+        num_timesteps (int): Number of timesteps that the varQITE will use.
+        Higher timesteps will result in more pression but will be slower.
+
+    Returns:
+        TimeEvolutionResult: This object has everything one needs to reproduce 
+        the expectation value and produce expectation values.
+        """
+    hamiltonian: OperatorBase = hamilton  
+    var_principle: VariationalPrinciple = ImaginaryMcLachlanPrinciple(
+        qgt=ReverseQGT(),
+        gradient=ReverseEstimatorGradient()
+    )
+    time: float = (1 / 2.0) * beta
+    aux_ops: List[OperatorBase] = [hamiltonian]
+    init_param_values: List[float] = [0] * (
+        depth * qubits + int(depth * qubits * (qubits - 1) / 2)
+    )
+    
+    evolution_problem: Any = TimeEvolutionProblem(
+        hamiltonian, 
+        time, 
+        aux_operators=aux_ops
+    )
+    var_qite: VarQITE = VarQITE(
+        ansatz, 
+        init_param_values, 
+        var_principle, 
+        Estimator(),
+        num_timesteps=num_timesteps
+    )
+    evolution_result: TimeEvolutionResult = var_qite.evolve(evolution_problem)
     return evolution_result
 
 
-def create_distribution_scipy(qubits, depth, hamilton, beta, ansatz):
+def create_distribution_scipy(qubits: int, depth: int, hamilton: OperatorBase,
+                              beta: float, ansatz: QuantumCircuit,
+                              num_timesteps: int) -> TimeEvolutionResult:
     """
-    creates the distribution via scipy, so not "quantumly"
-    :returns scipy calculated exp val, and the evolution object
-    """
-    hamiltonian =  hamilton #^ hm2.insert_i(qubits)
-    time = beta / 2.0
-    aux_ops = [hamiltonian]
-    #init_param_values = [np.pi / 2] * qubits + [0] * (depth * qubits * 2 + qubits)
-    init_param_values = [0] * (depth * qubits + int(depth*qubits*(qubits-1)/2))
-    init_state = Statevector(ansatz.assign_parameters(init_param_values))
-    evolution_problem = TimeEvolutionProblem(hamiltonian, time, initial_state=init_state, aux_operators=aux_ops)
-    exact_evol = SciPyImaginaryEvolver(num_timesteps=int(time * 600))
-    sol = exact_evol.evolve(evolution_problem)
-    #exact_h_exp_val = sol.observables[0][0].real
-    return  sol
+    Evolves the given initial state by scipy's imaginary time evolver.
+    Creates rho = exp(-beta * H)
+    Args:
+        qubits (int): The total numbers of qubits of the evolved system.
+        depth (int): depth
+        hamilton (OperatorBase): The Hamilton operator which will lead the 
+        evolution. 
+        beta (float): Inverse Temparature
+        ansatz (QuantumCircuit): The initial ansatz for the parameteritzed 
+        circuit
+        num_timesteps (int): Number of timesteps that the varQITE will use.
+        Higher timesteps will result in more pression but will be slower.
 
-
-def compare_results(evolution_result, sol, qubits, beta):
+    Returns:
+        TimeEvolutionResult: This object has everything one needs to reproduce 
+        the expectation value and produce expectation values.
     """
-    compares the results of varqite with computer
-    :param evolution_result: evolution result from varqite
-    :param sol: evolution result from scipy
-    :return: plots expvals varqite vs scipy, distributions varqite vs scipy
-    """
-    times = evolution_result.times
-    h_exp_val = np.array([ele[0][0] for ele in evolution_result.observables])
-    exact_h_exp_val = sol.observables[0][0].real
-    pylab.plot(times, h_exp_val, label="VarQITE")
-    pylab.plot(times, exact_h_exp_val, label="Exact", linestyle='--')
-    plt.axhline(y=0.25, color='r', linestyle='-')
-    pylab.xlabel("Time")
-    pylab.ylabel(r"$\langle H \rangle$ (energy)")
-    pylab.legend(loc="upper right")
-    plt.subplots()
-    evolved_state = evolution_result.evolved_state
-    varqite_result = Statevector(evolved_state).probabilities_dict()
-    scipy_result = Statevector(sol.evolved_state).probabilities_dict()
+    hamiltonian: OperatorBase = hamilton  
+    time: float = beta / 2.0
+    aux_ops: List[OperatorBase] = [hamiltonian]
+    init_param_values: List[float] = [0] * (
+        depth * qubits + int(depth * qubits * (qubits - 1) / 2)
+    )
     
+    init_state: Statevector = Statevector(
+        ansatz.assign_parameters(init_param_values))
+    
+    evolution_problem: Any = TimeEvolutionProblem(
+        hamiltonian, 
+        time, 
+        initial_state=init_state, 
+        aux_operators=aux_ops
+    )
+    
+    exact_evol: SciPyImaginaryEvolver = SciPyImaginaryEvolver(
+        num_timesteps=int(beta * num_timesteps)
+    )
+    sol: TimeEvolutionResult = exact_evol.evolve(evolution_problem)
+    return sol
+
+
+def plot_distribution(evolution_result: TimeEvolutionResult, 
+                      sol: TimeEvolutionResult,
+                      qubits: int, 
+                      depth: int, 
+                      a: float, 
+                      g: float,
+                      save: str = None, 
+                      filename: str = None) -> None:
+    """
+    Visualises the  distribution for given varQITE and scipy results. Only 
+    1*1 Matrix Models (just normal integrals) can be properly visualised.
+
+    Args:
+        evolution_result (TimeEvolutionResult): The distribution one would get 
+        from varQITE.
+        sol (TimeEvolutionResult): The distribution one would get from scipy.
+        qubits (int): Number of Qubits in the system.
+        depth (int): Depth of the circuit.
+        a (float): Lattice Spacing.
+        g (float): Interaction Constant.
+        save (str): The directory to save the visuals. Defaults to None.
+        filename (str): The name of the saved visual. Defaults to None.
+    """
+    
+    varqite_result = Statevector(
+                        evolution_result.evolved_state).probabilities_dict()
+    scipy_result = Statevector(sol.evolved_state).probabilities_dict()
     x = generate_combinations(qubits - 1)
     y = []
     y_2 = []
-    for i in range(len(x)):
+    for i in np.arange(len(x)):
         y.append(varqite_result[x[i]])
-    #for i in range(len(x)):
-       # y_2.append(scipy_result[x[i]])
-    
-    plt.bar(x, y, width=0.9, color = "red", label = "varqite")
-    #plt.bar(x, y_2, width=0.9, color = "blue", label = 'scipy')
+        y_2.append(scipy_result[x[i]])
+    plt.bar(x, y, color="red", label="varqite")
+    plt.bar(x, y_2, color="blue", label='scipy')
+    plt.xticks(ticks=range(len(x)), labels=x, rotation=90)
     plt.xlabel("Position")
     plt.ylabel("Probability")
-    plt.title("Comparison Scipy vs VarQITE with" + "" + str(beta))
+    plt.title("Scipy vs VarQITE: " + "Qubits= " + str(qubits) + " a= " + str(a) 
+              + " g= " + str(g) + " depth= " + str(depth))
     plt.legend()
-    plt.show()
-    """
-    save_directory = '/Users/salsa/MatrixModels/matrixmodels/remote/pictures/crankupto8'
-    file_name = "beta=" + str(beta) + ".png"
-    save_path = os.path.join(save_directory, file_name)
-    plt.savefig(save_path)
-    """
-
-def quick_compare(evolution_result, sol):
-    times = evolution_result.times
-    evolved_state = evolution_result.evolved_state
-    varqite_res = Statevector(evolved_state).probabilities_dict(np.arange(qubits))
-    scipy_result = Statevector(sol.evolved_state).probabilities_dict(np.arange(qubits))
-    h_exp_val = np.array([ele[0][0] for ele in varqite_res.observables])
-    plt.plot(times, h_exp_val, label = "varqite")
-    plt.plot(0.01*np.arange(len(scipy_result[0])), scipy_result[0],label = "scipy")
-    plt.legend()
-    difference = h_exp_val[-1]-scipy_result[0][-1]
-    print(difference)
+    
+    if save:
+        save_path = os.path.join(save, filename + ".png")
+        plt.savefig(save_path)
+        plt.show()
 
 
-def compare_results_exp(evolution_result, exact_h_exp_val):
+def quick_compare(evolution_result: TimeEvolutionResult, 
+                  sol: TimeEvolutionResult,
+                  qubits: int, 
+                  dimension: int, 
+                  depth: int, 
+                  a: float, 
+                  num_timesteps: int,
+                  g: float, 
+                  save: str = None, 
+                  filename: str = None) -> None:
     """
-    Compares the evolutions of ground state energies of scipy and varqite
-    """
-    times = evolution_result.times
+    This compares the evolution of the Energy <H> between the 
+    varQITE and scipy evolved distributions. The desired varQITE evolution 
+    would be close to the scipy one.
+
+    Args:
+       evolution_result (TimeEvolutionResult): The distribution one would get 
+        from varQITE.
+        sol (TimeEvolutionResult): The distribution one would get from scipy.
+        qubits (int): Number of Qubits in the system.
+        dimension (int): dimension of the matrix -1 . Dimension = 0 => 1*1 
+        matrix.
+        depth (int): Depth of the circuit.
+        a (float): Lattice Spacing.
+        g (float): Interaction Constant.
+        save (str): The directory to save the visuals. Defaults to None.
+        filename (str): The name of the saved visual. Defaults to None.
+        """
+    qubits_per_dim = int(qubits / (dimension + 1))
+    times = evolution_result.times[:(num_timesteps + 1)]
     h_exp_val = np.array([ele[0][0] for ele in evolution_result.observables])
-    pylab.plot(times, h_exp_val, label="VarQITE")
-    pylab.plot(times, exact_h_exp_val, label="Exact", linestyle='--')
-    pylab.xlabel("Time")
-    pylab.ylabel(r"$\langle H \rangle$ (energy)")
-    pylab.legend(loc="upper right")
-   
+    h_exp_val = h_exp_val[:(num_timesteps + 1)]
+    exact_h_exp_val = sol.observables[0][0].real[:(num_timesteps + 1)]
+    errors = []
+    for i in range(len(h_exp_val)):
+        error = h_exp_val[i] - exact_h_exp_val[i]
+        errors.append(error)
 
-def get_expval(binded, observable, qubits):
-    """
-    calculates the expextation value of the observable wrt. probability distribution
-    :param binded: e**bH that we get from varqite
-    :param observable: name of the observable
-    :param qubits: num qubits
-    :return: expectation value
-    """
-    estimator = Estimator()
-    op = observable ^ hm2.insert_i(qubits)
-    expectation_value = estimator.run(binded, op).result().values
-    return expectation_value
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(times, h_exp_val, label="VarQITE")
+    ax1.plot(times, exact_h_exp_val, label="Exact", linestyle='--', color="m")
+    ax2.plot(times, errors, linestyle="dotted", label="Error", color="r")
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel(r"$\langle H \rangle$ (energy)")
+    ax2.set_ylabel("Difference")
+    plt.title("Qubits per Dimension = " + str(qubits_per_dim) + " Dimension =" 
+              + str(dimension) + " Depth =" + str(depth) + " a =" + 
+              str(round(a, 3)) + " g =" + str(round(g, 3)))
+    ax1.legend(loc=2)
+    ax2.legend(loc=1)
+    plt.grid()
+    if save:
+        save_path = os.path.join(save, filename + ".png")
+        plt.savefig(save_path)
 
-
-def get_expval_shot(binded, observable, shots, qubits):
-    """
-    calculates the expextation value of the observable wrt. probability distribution
-    :param binded: e**bH that we get from varqite
-    :param observable: name of the observable
-    :param qubits: num qubits
-    :return: expectation value
-    """
-    estimator = Estimator()
-    op = observable ^ hm2.insert_i(qubits)
-    expectation_value = estimator.run(binded, op, shots=shots).result().values
-    return expectation_value
 
 if __name__ == "__main__":
     """
-    two_qubits = create_distribution_varqite(3, 1, np.kron(hm.Ham(3,1,2),hm.krId(3)), 1)
+    two_qubits = create_distribution_varqite(
+        3, 1, np.kron(hm.Ham(3,1,2),hm.krId(3)), 1)
     two_qubits_exp = create_distribution_scipy(3,1,hm.Ham(3,1,2),1)
     evolution_result = two_qubits[3]
     h_exp_val = two_qubits[2]
     exact_h_exp_val = two_qubits_exp[0]
     sol = two_qubits_exp[1]
-    compare_results(evolution_result, sol, h_exp_val , exact_h_exp_val, qubits = 3 , beta =)
+    compare_results(evolution_result, sol, h_exp_val , 
+    exact_h_exp_val, qubits = 3 , beta =)
 
 qubits = 3
 betas = [0.1, 0.5 , 0.75 , 1.0, 1.25 , 1.5]
 
 for i in betas:
-    two_qubits = create_distribution_varqite(qubits, 1, np.kron(hm.Ham(qubits,1,2),hm.krId(3)), i)
+    two_qubits = create_distribution_varqite(qubits, 1, 
+    np.kron(hm.Ham(qubits,1,2),hm.krId(3)), i)
     two_qubits_exp = create_distribution_scipy(qubits,1,hm.Ham(qubits,1,2), i)
     evolution_result = two_qubits[3]
     h_exp_val = two_qubits[2]
     exact_h_exp_val = two_qubits_exp[0]
     sol = two_qubits_exp[1]
-    compare_results(evolution_result, sol, h_exp_val , exact_h_exp_val, qubits , i)
+    compare_results(evolution_result, sol, h_exp_val , exact_h_exp_val, qubits 
+    , i)
     """
 """
 lambdas = []
